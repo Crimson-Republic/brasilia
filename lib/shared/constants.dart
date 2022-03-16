61f7ad27d4da54c2b30b658273cde5c2364db3ad
Othniel Ussher <othnielussher16@gmail.com>
2022-03-16 21:20:54 +0000

    Project setup

---
+++ lib/shared/constants.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Fonts {
+  static const String manrope = 'Manrope';
+}
+
+class Palette {
+  static const Color primary = Color(0xFF030530);
+}
+
+class Images {
+  static const String imagePath = 'assets/images/';
+  static const String rocket = imagePath + 'rocket.svg';
+  static const String gradient = imagePath + 'gradient.svg';
+}
