cabfdb4dfc9daa23ca6c01087b6a389893a32fd7
Othniel Ussher <othnielussher16@gmail.com>
2022-03-16 21:20:56 +0000

    Project setup

---
+++ lib/shared/exports.dart
@@ -0,0 +1,7 @@
+// pub
+export 'package:get/get.dart';
+export 'package:flutter_svg/svg.dart';
+
+// local
+export 'package:brasilia/screens/home.dart';
+export 'package:brasilia/shared/constants.dart';
