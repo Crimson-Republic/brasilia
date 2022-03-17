cb3713f0c5110202231968ccfabd5917df0ae88e
Othniel Ussher <othnielussher16@gmail.com>
2022-03-17 18:40:04 +0000

    Complete UI

--- lib/shared/exports.dart
+++ lib/shared/exports.dart
@@ -1,7 +1,22 @@
 // pub
 export 'package:get/get.dart';
 export 'package:flutter_svg/svg.dart';
+export 'package:responsive_builder/responsive_builder.dart';
 
 // local
 export 'package:brasilia/screens/home.dart';
+export 'package:brasilia/widgets/header.dart';
 export 'package:brasilia/shared/constants.dart';
+export 'package:brasilia/screens/result_page.dart';
+export 'package:brasilia/widgets/hero_text.dart';
+export 'package:brasilia/screens/first_page.dart';
+export 'package:brasilia/widgets/input_section.dart';
+export 'package:brasilia/widgets/generate_button.dart';
+export 'package:brasilia/widgets/background_color.dart';
+export 'package:brasilia/widgets/foreground_color.dart';
+
+// controllers
+export 'package:brasilia/controllers/home_controller.dart';
+export 'package:brasilia/controllers/final_color_controller.dart';
+export 'package:brasilia/controllers/background_color_controller.dart';
+export 'package:brasilia/controllers/foreground_color_controller.dart';
