ec33d3e93ed8844d0f6ec70e8d5f2638547b1432
Othniel Ussher <othnielussher16@gmail.com>
2022-03-17 18:40:02 +0000

    Complete UI

--- lib/shared/constants.dart
+++ lib/shared/constants.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
 
+enum colorType { foreground, background }
+
 class Fonts {
   static const String manrope = 'Manrope';
 }
 
 class Palette {
   static const Color primary = Color(0xFF030530);
+  static const Color scaffold = Color(0xFFF9F9F9);
 }
 
 class Images {
   static const String imagePath = 'assets/images/';
   static const String rocket = imagePath + 'rocket.svg';
   static const String gradient = imagePath + 'gradient.png';
+  static const String copy = imagePath + 'copy.svg';
 }
 
 class Doubles {
@@ -22,3 +26,7 @@ String fastestWay =
     'Fastest way to discover the color\ncode for your applied opacity';
 
 String justEnter = 'Just enter your color code and opacity value. BOOOM✨';
+
+String copyrightStatement = '© 2022 by Opacity. All rights reserved';
+
+String developedBy = 'Designed and Developed by Crimson Republic';
