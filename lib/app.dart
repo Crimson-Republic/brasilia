34fbba9bdb52b8a794eeffba5d3e7fb941c021b9
Othniel Ussher <othnielussher16@gmail.com>
2022-03-17 18:39:39 +0000

    Complete UI

--- lib/app.dart
+++ lib/app.dart
@@ -1,16 +1,34 @@
 import 'package:flutter/material.dart';
 import 'package:brasilia/shared/exports.dart';
 
-class App extends StatelessWidget {
+class App extends StatefulWidget {
   const App({Key? key}) : super(key: key);
+
+  @override
+  State<App> createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  @override
+  void initState() {
+    super.initState();
+    Get.put(HomeController());
+    Get.put(FinalColorController());
+    Get.put(ForegroundColorController());
+    Get.put(BackgroundColorController());
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: const Home(),
+      home: Home(),
       title: 'Opacitié Demo',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(primarySwatch: Colors.blue, fontFamily: Fonts.manrope),
+      theme: ThemeData(
+        fontFamily: Fonts.manrope,
+        primarySwatch: Colors.blue,
+        scaffoldBackgroundColor: Palette.scaffold,
+      ),
     );
   }
 }
