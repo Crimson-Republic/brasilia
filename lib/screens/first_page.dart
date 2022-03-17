74cc9af106bdc1856e7954a4e7f2fc6d916976b3
Othniel Ussher <othnielussher16@gmail.com>
2022-03-17 18:39:55 +0000

    Complete UI

---
+++ lib/screens/first_page.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+import 'package:brasilia/shared/exports.dart';
+
+class FirstPage extends StatelessWidget {
+  const FirstPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: Get.height,
+      padding: const EdgeInsets.symmetric(horizontal: Doubles.horizontalMargin),
+      decoration: const BoxDecoration(
+        image: DecorationImage(
+          fit: BoxFit.cover,
+          image: ExactAssetImage(Images.gradient),
+        ),
+      ),
+      child: ScrollConfiguration(
+        behavior: ScrollConfiguration.of(context).copyWith(scrollbars: false),
+        child: SingleChildScrollView(
+          child: Column(
+            children: const [
+              Padding(
+                padding: EdgeInsets.only(top: 28.0),
+                child: Header(),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 60.0),
+                child: HeroText(),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 40.0),
+                child: InputSection(),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: 32.0, bottom: 40.0),
+                child: GenerateButton(),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
