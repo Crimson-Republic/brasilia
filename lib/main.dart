724b75c821b4f56e1c6f8432715003868b429df2
Othniel Ussher <othnielussher16@gmail.com>
2022-03-19 20:43:54 +0000

    Update foreground input

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,8 @@
 import 'package:brasilia/app.dart';
 import 'package:flutter/material.dart';
+import 'package:url_strategy/url_strategy.dart';
 
-void main() => runApp(const App());
-
+void main() {
+  setPathUrlStrategy();
+  runApp(const App());
+}
