75f554861704d6fa05921a1a095f8b057f8caf5f
Othniel Ussher <othnielussher16@gmail.com>
2022-03-19 20:44:09 +0000

    Update foreground input

--- lib/widgets/generate_button.dart
+++ lib/widgets/generate_button.dart
@@ -21,44 +21,48 @@ class _GenerateButtonState extends State<GenerateButton> {
       focusColor: Colors.transparent,
       splashColor: Colors.transparent,
       highlightColor: Colors.transparent,
-      child: AnimatedContainer(
-        curve: Curves.fastLinearToSlowEaseIn,
-        duration: const Duration(milliseconds: 800),
-        padding: EdgeInsets.symmetric(
-          vertical: 14.0,
-          horizontal: generating ? 14.0 : 68.0,
-        ),
-        decoration: BoxDecoration(
-          color: Palette.primary,
-          borderRadius: BorderRadius.circular(generating ? 100.0 : 4.0),
-        ),
-        child: generating
-            ? const SizedBox(
-                height: 24.0,
-                width: 24.0,
-                child: CircularProgressIndicator(
-                  strokeWidth: 3.0,
-                  color: Colors.white,
-                ),
-              )
-            : Row(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  SvgPicture.asset(Images.rocket),
-                  const SizedBox(width: 12.0),
-                  const Text(
-                    'Generate',
-                    style: TextStyle(fontSize: 20.0, color: Colors.white),
+      child: Obx(
+        () => AnimatedContainer(
+          curve: Curves.fastLinearToSlowEaseIn,
+          duration: const Duration(milliseconds: 800),
+          padding: EdgeInsets.symmetric(
+            vertical: 14.0,
+            horizontal: generating ? 14.0 : 68.0,
+          ),
+          decoration: BoxDecoration(
+            color: colorController.foreground.value.length == 6
+                ? Palette.primary
+                : Colors.grey,
+            borderRadius: BorderRadius.circular(generating ? 100.0 : 4.0),
+          ),
+          child: generating
+              ? const SizedBox(
+                  height: 24.0,
+                  width: 24.0,
+                  child: CircularProgressIndicator(
+                    strokeWidth: 3.0,
+                    color: Colors.white,
                   ),
-                ],
-              ),
+                )
+              : Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    SvgPicture.asset(Images.rocket),
+                    const SizedBox(width: 12.0),
+                    const Text(
+                      'Generate',
+                      style: TextStyle(fontSize: 20.0, color: Colors.white),
+                    ),
+                  ],
+                ),
+        ),
       ),
     );
   }
 
   Future<void> generate() async {
     setState(() => generating = true);
-    await Future.delayed(const Duration(seconds: 4));
+    await Future.delayed(const Duration(seconds: 2));
     setState(() => generating = false);
     colorController.generateColor();
     colorController.copiedToClipboard.value = false;
