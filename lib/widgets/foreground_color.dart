b5b8a476ee09d02a8837ec6861372b4cc02467f5
Othniel Ussher <othnielussher16@gmail.com>
2022-04-05 23:23:10 +0000

    🔨 Fix color mixing bug

--- lib/widgets/foreground_color.dart
+++ lib/widgets/foreground_color.dart
@@ -28,65 +28,48 @@ class ForegroundColor extends StatelessWidget {
           child: Row(
             mainAxisSize: MainAxisSize.min,
             children: [
-              Obx(
-                () => Container(
-                  width: 40.0,
-                  height: 40.0,
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(2.0),
-                    // color: colorController.foreground.isEmpty
-                    //     ? const Color(0xFF9E938C).withOpacity(
-                    //         colorController.foregroundOpacity.value > 100
-                    //             ? 1.0
-                    //             : colorController.foregroundOpacity.value / 100,
-                    //       )
-                    //     : Color(
-                    //         int.parse(
-                    //           '0xFF${colorController.foreground.value.split('#').last}',
-                    //         ),
-                    //       ).withOpacity(
-                    //         colorController.foregroundOpacity.value > 100
-                    //             ? 1.0
-                    //             : colorController.foregroundOpacity.value / 100,
-                    //       ),
-                    color: colorController.foregroundColor(),
-                  ),
-                ),
+              GetBuilder<ColorController>(
+                builder: ((controller) {
+                  return Container(
+                    width: 40.0,
+                    height: 40.0,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(2.0),
+                      color: controller.foreground.isEmpty
+                          ? const Color(0xFF9E938C).withOpacity(
+                              controller.foregroundOpacity,
+                            )
+                          : resolveColor(),
+                    ),
+                  );
+                }),
               ),
               const SizedBox(width: 16.0),
               SizedBox(
                 width: 95.0,
-                child: Obx(
-                  () => TextField(
-                    maxLength: colorController.foreground.value.startsWith('#')
-                        ? 7
-                        : 6,
-                    autofocus: true,
-                    autocorrect: false,
-                    cursorColor: Palette.primary,
-                    style: const TextStyle(
-                      fontSize: 24.0,
-                      color: Palette.primary,
-                    ),
-                    onChanged: (value) {
-                      if (value.startsWith('#')) {
-                        colorController.foreground.value =
-                            value.trim().substring(0, 6);
-                      } else {
-                        colorController.foreground.value =
-                            value.trim().substring(0, 5);
-                      }
-                    },
-                    decoration: InputDecoration(
-                      hintText: '9E938C',
-                      border: InputBorder.none,
-                      counter: const SizedBox.shrink(),
-                      hintStyle: TextStyle(
+                child: GetBuilder<ColorController>(
+                  builder: (controller) {
+                    return TextField(
+                      autofocus: true,
+                      autocorrect: false,
+                      cursorColor: Palette.primary,
+                      maxLength: controller.foregroundStartsWithHash ? 7 : 6,
+                      style: const TextStyle(
                         fontSize: 24.0,
-                        color: Palette.primary.withOpacity(0.2),
+                        color: Palette.primary,
                       ),
-                    ),
-                  ),
+                      onChanged: (value) => onHexFieldChange(value),
+                      decoration: InputDecoration(
+                        hintText: '9E938C',
+                        border: InputBorder.none,
+                        counter: const SizedBox.shrink(),
+                        hintStyle: TextStyle(
+                          fontSize: 24.0,
+                          color: Palette.primary.withOpacity(0.2),
+                        ),
+                      ),
+                    );
+                  },
                 ),
               ),
               const SizedBox(width: 24.0),
@@ -106,14 +89,7 @@ class ForegroundColor extends StatelessWidget {
                       fontSize: 20.0,
                       color: Palette.primary,
                     ),
-                    onChanged: (value) {
-                      if (value.isNotEmpty) {
-                        colorController.foregroundOpacity.value =
-                            int.parse(value.trim());
-                      } else {
-                        colorController.foregroundOpacity.value = 100;
-                      }
-                    },
+                    onChanged: (value) => onOpacityChange(value),
                     decoration: InputDecoration(
                       isDense: true,
                       hintText: '100%',
@@ -133,5 +109,41 @@ class ForegroundColor extends StatelessWidget {
         ),
       ],
     );
+  }
+
+  Color resolveColor() {
+    try {
+      return Color(
+        int.parse(
+          '0xFF${colorController.foreground.split('#').last}',
+        ),
+      ).withOpacity(colorController.foregroundOpacity);
+    } catch (error) {
+      return const Color(0xFF9E938C);
+    }
+  }
+
+  void onHexFieldChange(String value) {
+    if (value.startsWith('#')) {
+      colorController.foreground = value.trim().substring(1);
+      colorController.setForegroundStartsWithHash(true);
+    } else {
+      colorController.foreground = value.trim();
+      colorController.updateForeground(value);
+      colorController.setForegroundStartsWithHash(false);
+    }
+  }
+
+  void onOpacityChange(String value) {
+    try {
+      if (value.isNotEmpty) {
+        double opacity = (int.parse(value.trim()) / 100);
+        colorController.updateForegroundOpacity(opacity);
+      } else {
+        colorController.updateForegroundOpacity(1.0);
+      }
+    } catch (error) {
+      colorController.updateForegroundOpacity(1.0);
+    }
   }
 }
