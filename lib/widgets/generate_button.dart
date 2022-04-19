dc42896aeed79ad2e884e0d146d3d54db581fe29
Othniel Ussher <othnielussher16@gmail.com>
2022-04-19 01:49:04 +0000

    📱 Add responsiveness

--- lib/widgets/generate_button.dart
+++ lib/widgets/generate_button.dart
@@ -23,55 +23,66 @@ class _GenerateButtonState extends State<GenerateButton> {
         highlightColor: Colors.transparent,
         child: GetBuilder<ColorController>(
           builder: ((controller) {
-            return AnimatedContainer(
-              curve: Curves.fastLinearToSlowEaseIn,
-              duration: const Duration(milliseconds: 800),
-              padding: EdgeInsets.symmetric(
-                vertical: 14.0,
-                horizontal: generating ? 14.0 : 68.0,
-              ),
-              decoration: BoxDecoration(
-                color: controller.foregroundStartsWithHash
-                    ? controller.foreground.length == 7
-                        ? Palette.primary
-                        : Colors.grey
-                    : controller.foreground.length == 6
+            return ResponsiveBuilder(
+              builder: (context, sizingInformation) {
+                bool isDesktop = sizingInformation.isDesktop;
+                return AnimatedContainer(
+                  alignment: Alignment.center,
+                  curve: Curves.fastLinearToSlowEaseIn,
+                  width: isDesktop ? 260.0 : double.infinity,
+                  duration: const Duration(milliseconds: 800),
+                  padding: EdgeInsets.symmetric(
+                    vertical: 14.0,
+                    horizontal: generating ? 14.0 : 68.0,
+                  ),
+                  decoration: BoxDecoration(
+                    color: (controller.foreground.length == 6 &&
+                            controller.foregroundTextController.text.isNotEmpty)
                         ? Palette.primary
                         : Colors.grey,
-                borderRadius: BorderRadius.circular(generating ? 100.0 : 4.0),
-              ),
-              child: generating
-                  ? const SizedBox(
-                      height: 24.0,
-                      width: 24.0,
-                      child: CircularProgressIndicator(
-                        strokeWidth: 3.0,
-                        color: Colors.white,
-                      ),
-                    )
-                  : Row(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        SvgPicture.asset(Images.rocket),
-                        const SizedBox(width: 12.0),
-                        const Text(
-                          'Generate',
-                          style: TextStyle(fontSize: 20.0, color: Colors.white),
+                    borderRadius:
+                        BorderRadius.circular(generating ? 100.0 : 4.0),
+                  ),
+                  child: generating
+                      ? const SizedBox(
+                          width: 24.0,
+                          height: 24.0,
+                          child: CircularProgressIndicator(
+                            strokeWidth: 3.0,
+                            color: Colors.white,
+                          ),
+                        )
+                      : Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            SvgPicture.asset(Images.rocket),
+                            const SizedBox(width: 12.0),
+                            const Text(
+                              'Generate',
+                              style: TextStyle(
+                                fontSize: 20.0,
+                                color: Colors.white,
+                              ),
+                            ),
+                          ],
                         ),
-                      ],
-                    ),
+                );
+              },
             );
           }),
         ));
   }
 
   Future<void> generate() async {
-    setState(() => generating = true);
-    await Future.delayed(const Duration(seconds: 2));
-    setState(() => generating = false);
-    colorController.generateColor();
-    colorController.copiedToClipboard.value = false;
-    await Future.delayed(const Duration(seconds: 1));
-    homeController.scrollToBottom();
+    if ((colorController.foreground.length == 6 &&
+        colorController.foregroundTextController.text.isNotEmpty)) {
+      setState(() => generating = true);
+      await Future.delayed(const Duration(seconds: 2));
+      setState(() => generating = false);
+      colorController.generateColor();
+      colorController.copiedToClipboard.value = false;
+      await Future.delayed(const Duration(seconds: 1));
+      homeController.scrollToBottom();
+    }
   }
 }
