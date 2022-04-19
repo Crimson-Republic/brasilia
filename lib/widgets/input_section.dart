d49625ad6f729f9ed194c26f85c0860e32c2afe4
Othniel Ussher <othnielussher16@gmail.com>
2022-04-19 01:49:11 +0000

    📱 Add responsiveness

--- lib/widgets/input_section.dart
+++ lib/widgets/input_section.dart
@@ -6,24 +6,32 @@ class InputSection extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.symmetric(
-        vertical: 34.0,
-        horizontal: 56.0,
-      ),
-      decoration: BoxDecoration(
-        color: Colors.white.withOpacity(0.2),
-        borderRadius: BorderRadius.circular(20.0),
-        border: Border.all(color: Colors.white),
-      ),
-      child: Row(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Flexible(child: ForegroundColor()),
-          const SizedBox(width: 40.0),
-          Flexible(child: BackgroundColor()),
-        ],
-      ),
+    return ResponsiveBuilder(
+      builder: (context, sizingInformation) {
+        bool isDesktop = sizingInformation.isDesktop;
+        return Container(
+          padding: EdgeInsets.symmetric(
+            vertical: 34.0,
+            horizontal: isDesktop ? 56.0 : 20.0,
+          ),
+          decoration: BoxDecoration(
+            color: Colors.white.withOpacity(0.2),
+            borderRadius: BorderRadius.circular(20.0),
+            border: Border.all(color: Colors.white),
+          ),
+          child: Flex(
+            mainAxisSize: MainAxisSize.min,
+            direction: isDesktop ? Axis.horizontal : Axis.vertical,
+            children: [
+              Flexible(child: ForegroundColor()),
+              isDesktop
+                  ? const SizedBox(width: 40.0)
+                  : const SizedBox(height: 40.0),
+              Flexible(child: BackgroundColor()),
+            ],
+          ),
+        );
+      },
     );
   }
 }
