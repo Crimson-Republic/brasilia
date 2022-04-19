0c65d4670189e0533a86294aa7d118bcfb3ae407
Othniel Ussher <othnielussher16@gmail.com>
2022-04-19 01:48:47 +0000

    📱 Add responsiveness

--- lib/screens/first_page.dart
+++ lib/screens/first_page.dart
@@ -6,34 +6,45 @@ class FirstPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: Get.height,
-      padding: const EdgeInsets.symmetric(horizontal: Doubles.horizontalMargin),
-      decoration: const BoxDecoration(
-        image: DecorationImage(
-          fit: BoxFit.cover,
-          image: ExactAssetImage(Images.gradient),
-        ),
-      ),
-      child: ScrollConfiguration(
-        behavior: ScrollConfiguration.of(context).copyWith(scrollbars: false),
-        child: SingleChildScrollView(
-          child: Column(
-            children: const [
-              Padding(padding: EdgeInsets.only(top: 28.0), child: Header()),
-              Padding(padding: EdgeInsets.only(top: 60.0), child: HeroText()),
-              Padding(
-                padding: EdgeInsets.only(top: 40.0),
-                child: InputSection(),
-              ),
-              Padding(
-                padding: EdgeInsets.only(top: 32.0, bottom: 40.0),
-                child: GenerateButton(),
-              ),
-            ],
+    return ResponsiveBuilder(
+      builder: (context, sizingInformation) {
+        bool isDesktop = sizingInformation.isDesktop;
+        return Container(
+          height: Get.height,
+          padding: EdgeInsets.symmetric(
+            horizontal: isDesktop
+                ? Doubles.horizontalMargin
+                : Doubles.horizontalMarginMobile,
           ),
-        ),
-      ),
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              fit: BoxFit.cover,
+              image: ExactAssetImage(Images.gradient),
+            ),
+          ),
+          child: ScrollConfiguration(
+            behavior:
+                ScrollConfiguration.of(context).copyWith(scrollbars: false),
+            child: SingleChildScrollView(
+              child: Column(
+                children: const [
+                  Padding(padding: EdgeInsets.only(top: 28.0), child: Header()),
+                  Padding(
+                      padding: EdgeInsets.only(top: 60.0), child: HeroText()),
+                  Padding(
+                    padding: EdgeInsets.only(top: 40.0),
+                    child: InputSection(),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(top: 32.0, bottom: 40.0),
+                    child: GenerateButton(),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      },
     );
   }
 }
